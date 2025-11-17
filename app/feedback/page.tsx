235789c28c7894a39851b454130f4140573a8356
ishaanbusi <ishaanbusi@gmail.com>
2025-11-17 18:34:28 +0530

    updates

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -396,7 +396,7 @@ export default function FeedbackPage() {
                 />
               </div>
 
-              {/* Submit Button */}
+              {/* Submit Button m */}
               <button
                 type="submit"
                 disabled={isSubmitting}
